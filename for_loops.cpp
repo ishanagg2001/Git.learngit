1af47bf2771b6ccf17d86478febba54b32384a37
Ishan Agarwal <ishanagg2001@gmail.com>
2021-02-22 02:35:21 +0530

    loops file modified

--- for_loops.cpp
+++ for_loops.cpp
@@ -5,7 +5,7 @@ int main()
 	for (int i = 0; i < 10; i++)
 		cout << "ishan";
 	cout << "cricket lovers\n";
-	cout << "New branch\n";
+	cout << "New branch\n\n";
 	cout << "2nd new branch xyz\n";
 	return 0;
 }
